c8f5b98468fcb5a9d7826ef49992c42f7ad0a3e4
Trayan Azarov <trayan.azarov@amikos.tech>
2024-05-12 12:03:40 +0300

    fix: Runt perf test py script

--- perf_test/run_perf_test.py
+++ perf_test/run_perf_test.py
@@ -4,7 +4,7 @@ import subprocess
 import json
 
 
-def run_locust_with_env(jsonl_file):
+def run_locust_with_env(jsonl_file, output_dir):
     with open(jsonl_file, 'r') as file:
         for line in file:
             # Convert JSON line to a dictionary
@@ -16,16 +16,20 @@ def run_locust_with_env(jsonl_file):
             # Run locust using subprocess
             print(f"Starting single user test for : {data['id']}")
             command = ['poetry', 'run', 'locust', '-f', 'perf_test/locust_t.py', '--headless', '-u', '1', '-r', '1',
-                       '--run-time', os.getenv("PERF_TEST_DURATION", "1m"), '--csv=results_${size}']
+                       '--run-time', os.getenv("PERF_TEST_DURATION", "1m"),
+                       f"--csv={output_dir}/results_single_${data['id']}"]
             subprocess.run(command, env=env)
             print(f"Starting multi user (5) test for : {data['id']}")
             command = ['poetry', 'run', 'locust', '-f', 'perf_test/locust_t.py', '--headless', '-u',
                        os.getenv("PERF_TEST_CONCURRENT_USERS", "5"), '-r', '1',
-                       '--run-time', os.getenv("PERF_TEST_DURATION", "1m"), f"--csv=results_${data['id']}"]
+                       '--run-time', os.getenv("PERF_TEST_DURATION", "1m"),
+                       f"--csv={output_dir}/results_multi_${data['id']}"]
             subprocess.run(command, env=env)
 
 
 if __name__ == "__main__":
     ar = argparse.ArgumentParser()
     ar.add_argument("--queries-file", type=str, required=True, help="Path to the queries JSONL file")
-    run_locust_with_env(ar.parse_args().jsonl_file)
+    ar.add_argument("--output-path", type=str, required=True, help="Path to the output directory")
+    a = ar.parse_args()
+    run_locust_with_env(a.queries_file, a.output_path)
