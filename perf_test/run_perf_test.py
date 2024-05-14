4a2c1e74783e6162f0fee8d342fa82d2815daf02
Trayan Azarov <trayan.azarov@amikos.tech>
2024-05-14 20:09:58 +0300

    chore: Removing periodic summaries from console logs

--- perf_test/run_perf_test.py
+++ perf_test/run_perf_test.py
@@ -14,14 +14,21 @@ def run_locust_with_env(jsonl_file, output_dir):
             # Set the environment variable
             env = {"LOCUST_JSON": env_var, "PATH": f"{os.getenv('HOME')}/.local/bin:{os.getenv('PATH')}"}
             # Run locust using subprocess
-            print(f"Starting single user test for : {data['id']}")
-            command = ['poetry', 'run', 'locust', '-f', 'perf_test/locust_t.py', '--headless', '-u', '1', '-r', '1',
+            command = ['poetry', 'run', 'locust',
+                       '-f', 'perf_test/locust_t.py',
+                       '--headless',
+                       '--only-summary',
+                       '-u', '1',
+                       '-r', '1',
                        '--run-time', os.getenv("PERF_TEST_DURATION", "1m"),
                        f"--csv={output_dir}/results_single_${data['id']}"]
             subprocess.run(command, env=env)
-            print(f"Starting multi user (5) test for : {data['id']}")
-            command = ['poetry', 'run', 'locust', '-f', 'perf_test/locust_t.py', '--headless', '-u',
-                       os.getenv("PERF_TEST_CONCURRENT_USERS", "5"), '-r', '1',
+            command = ['poetry', 'run', 'locust',
+                       '-f', 'perf_test/locust_t.py',
+                       '--headless',
+                       '--only-summary',
+                       '-u', os.getenv("PERF_TEST_CONCURRENT_USERS", "5"),
+                       '-r', '1',
                        '--run-time', os.getenv("PERF_TEST_DURATION", "1m"),
                        f"--csv={output_dir}/results_multi_${data['id']}"]
             subprocess.run(command, env=env)
